9aa84fd54b62cd3d55c3decc2dd6cca2a88022d1
shuhelahm <shuhel037@gmail.com>
2022-03-01 01:56:01 +0600

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <section>
+            {/* banner section */}
+
+            <div>
+                <img className='w-100' style={{height: "350px"}} src="https://images.pexels.com/photos/6043246/pexels-photo-6043246.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+            </div>
+        </section>
+    );
+};
+
+export default Home;
