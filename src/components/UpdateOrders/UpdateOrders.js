ad4e50282057d470bf8955afa28b0b09a9a6e063
shuhelahm <shuhel037@gmail.com>
2022-08-18 00:55:25 +0600

    update order route created

--- src/components/UpdateOrders/UpdateOrders.js
+++ src/components/UpdateOrders/UpdateOrders.js
@@ -6,11 +6,11 @@ const UpdateOrders = () => {
     const addressRef = useRef()
     const phoneRef = useRef()
     const {id} = useParams();
-    const address = addressRef?.current?.value;
-    const phone = addressRef?.current?.value;
-    const updatedOrder = (address, phone)
-
-    fetch(`https://guarded-inlet-05054.herokuapp.com/orders/${id}`,{
+    const handleUpdate = id =>{
+        const address = addressRef?.current?.value;
+        const phone = phoneRef?.current?.value;
+        const updatedOrder = {address, phone}
+        fetch(`https://guarded-inlet-05054.herokuapp.com/orders/${id}`,{
         method: 'PUT',
         headers: {
           'content-type': 'application/json'
@@ -20,15 +20,18 @@ const UpdateOrders = () => {
     )
     .then(res=>res.json())
     .then(data=> {
-        alert('order update success')
+        alert('Order details updated')
     })
+    }
+    
+    
 
     return (
         <div>
             <h1>Order ID: {id}</h1>
             <p>Adress: <input type="text" ref={addressRef}/></p>
             <p>Phone: <input type="text" ref={phoneRef}/></p>
-            <button onClick={UpdateOrders}>Update</button>
+            <button onClick={()=>handleUpdate(id)}>Update</button>
         </div>
     );
 };
