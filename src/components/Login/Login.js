d3370e5b8aedcf50ab003b907d4c462586d67e13
Shuhel Ahmed <shuhel020@gmail.com>
2023-11-15 23:04:30 -0500

    email&pass register added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,11 +4,22 @@ import initializeAuthentication from '../Firebase/firebase.initialize';
 initializeAuthentication();
 
 const Login = () => {
-    const {handleGoogleLogIn} = useAuth();
+    const {handleGoogleLogIn, handleEmail, handlePassword, handleRegister, error} = useAuth();
     return (
+        <>
+        <section>
+            <form onSubmit={handleRegister}>
+            <input onBlur={handleEmail} placeholder='email'></input>
+            <input onBlur={handlePassword} placeholder='password'></input>
+            <button type="submit">Submit</button>
+            </form>
+            <h5>{error}</h5>
+        </section>
         <section className='text-center my-4'>
+            
             <button onClick={handleGoogleLogIn} className='bg-orange-400 p-2 hover:bg-orange-600 rounded'>Login using Google</button>
         </section>
+        </>
     );
 };
 
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,4 +1,4 @@
-import {  signInWithPopup, GoogleAuthProvider, getAuth, onAuthStateChanged,signOut } from "firebase/auth";
+import {  createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, getAuth, onAuthStateChanged,signOut } from "firebase/auth";
 import { useEffect, useState } from "react";
 import initializeAuthentication from "../components/Firebase/firebase.initialize";
 
@@ -7,8 +7,33 @@ initializeAuthentication();
 
 const useFirebase = () =>{
     const [user, setUser] = useState({});
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
     const [isLoading, setIsLoading] = useState(true);
     const auth = getAuth();
+
+    const handleEmail = e =>{
+        setEmail(e.target.value)
+    }
+    const handlePassword = e =>{
+        setPassword(e.target.value)
+    }
+
+    const handleRegister = e =>{
+        console.log('clicked')
+        e.preventDefault();
+       
+        createUserWithEmailAndPassword(auth,email,password)
+        .then(userCredential => {
+            const user = userCredential.user ;
+            setError('')
+            e.target.reset();
+        })
+        .catch(error=>{
+            setError(error.message)
+        })
+    }
 
     const handleGoogleLogIn = () =>{
         setIsLoading(true);
@@ -19,6 +44,8 @@ const useFirebase = () =>{
         })
         .finally(()=>setIsLoading(false))
     }
+
+    
 
     const logout = () =>{
         setIsLoading(true)
@@ -43,10 +70,16 @@ const useFirebase = () =>{
     },[])
 
     return{
+        email,
+        handleEmail,
+        password,
+        handlePassword,
+        handleRegister,
         user,
         logout,
         handleGoogleLogIn,
-        isLoading
+        isLoading,
+        error
     }
 }
 
